9b3644824c456bf92b2ff2479aefbaf58ab4c63d
olowojude <olowojude73@gmail.com>
2022-09-13 23:16:50 +0100

    Revert "Wrote the tests for the urls.py"
    This reverts commit a8aae4149674f0cd8d42a4b61068fae8a1536fd5.

--- ba/forms.py
+++ ba/forms.py
@@ -2,3 +2,7 @@ from django.forms import ModelForm
 from .models import Post, Comments
        
              
+#class CommentsForm(ModelForm):
+#    class Meta:       
+#       model = Comments
+#       fields = ["name", "email", "body"]
