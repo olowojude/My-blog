a8aae4149674f0cd8d42a4b61068fae8a1536fd5
olowojude <olowojude73@gmail.com>
2022-09-13 22:23:05 +0100

    Wrote the tests for the urls.py

--- ba/forms.py
+++ ba/forms.py
@@ -2,7 +2,3 @@ from django.forms import ModelForm
 from .models import Post, Comments
        
              
-#class CommentsForm(ModelForm):
-#    class Meta:       
-#       model = Comments
-#       fields = ["name", "email", "body"]
