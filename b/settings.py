7b99fcce1a7f3b9c131ff0c262bdcaf11c415a4b
olowojude <olowojude73@gmail.com>
2022-09-17 19:44:03 +0100

    Update settings.py

--- b/settings.py
+++ b/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = config('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = ['https://jude-olowo-blog.up.railway.app/']
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
