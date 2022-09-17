183c6bad7a4d2f638737ce5be45850716624b013
olowojude <olowojude73@gmail.com>
2022-09-17 19:34:40 +0100

    Update settings.py

--- b/settings.py
+++ b/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = config('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = ["*"]
+ALLOWED_HOSTS = ['https://jude-olowo-blog.up.railway.app/']
 
 
 # Application definition
@@ -152,8 +152,9 @@ EMAIL_BACKEND = "django.core.mail.backends.smtp.EmailBackend"
 
 
 # security
+CSRF_TRUSTED_ORIGINS = ['https://jude-olowo-blog.up.railway.app/']
 # SESSION_COOKIE_SECURE = True
-# CSRF_COOKIE_SECURE = True 
+CSRF_COOKIE_SECURE = True 
 
 # SECURE_HSTS_SECONDS = 31536000
 # SECURE_HSTS_INCLUDE_SUBDOMAINS = True
